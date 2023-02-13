dca8800607c509bf7573d2a79aae1e655d4a4176
SUDHEER341 <sudheer601220@gmail.com>
2023-02-13 16:59:01 +0530

    Refactor : Store the Daily Wage along with the Total Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,5 +1,6 @@
 package com.employeewage;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 interface IEmployeeWageComputation
 {
@@ -10,7 +11,7 @@ interface IEmployeeWageComputation
 
 class CompanyEmpWage
 {
-
+    // instance constants
     final String COMPANY_NAME;
     final int WAGE_PER_HR;
     final int MAX_WORKING_DAYS;
@@ -46,21 +47,24 @@ class CompanyEmpWage
 
 class EmployeeWageComputation implements IEmployeeWageComputation
 {
-    
+    // class constants
     public static final int PART_TIME = 1;
     public static final int FULL_TIME = 2;
     // instance variables
     ArrayList<CompanyEmpWage> companies;
+    HashMap<String, Integer> totalEmpWages;
 
     public EmployeeWageComputation()
     {
         companies = new ArrayList<>();
+        totalEmpWages = new HashMap<>();
     }
 
     public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs)
     {
         CompanyEmpWage company = new CompanyEmpWage(companyName, wagePerHr, maxWorkingDays, maxWorkingHrs);
         companies.add(company);
+        totalEmpWages.put(companyName,0);
     }
 
     int generateEmployeeType()
@@ -107,7 +111,18 @@ class EmployeeWageComputation implements IEmployeeWageComputation
             totalWage += wage;
             System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHrs, wage, totalWorkingHrs + workingHrs);
         }
+        totalEmpWages.put(companyEmpWage.COMPANY_NAME, totalWage);
         return totalWage;
+    }
+
+    void printTotalEmpWages()
+    {
+        System.out.println("The Companies and their total Employee Wages are:");
+        for (String companyName : totalEmpWages.keySet())
+        {
+            System.out.println(companyName + ": " + totalEmpWages.get(companyName));
+        }
+
     }
 
     public static void main(String args[])
@@ -115,8 +130,8 @@ class EmployeeWageComputation implements IEmployeeWageComputation
         EmployeeWageComputation employeeWageComputation = new EmployeeWageComputation();
         employeeWageComputation.addCompany("Microsoft", 4, 30, 100);
         employeeWageComputation.addCompany("Google", 5, 40, 170);
-        employeeWageComputation.addCompany("Apple", 9, 10, 70);
         employeeWageComputation.addCompany("Amazon", 19, 10, 150);
         employeeWageComputation.calculateTotalWage();
+        employeeWageComputation.printTotalEmpWages();
     }
 }
